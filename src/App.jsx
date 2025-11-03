926056caefa37ac2f560d8b1b1a5135fd9a99e32
Nizam Uddin Nayeem <nizamuddinnayeem22@gmail.com>
2025-11-03 12:46:38 +0600

    First react app commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+
+      <h1 style={{ 
+        textAlign: 'center',
+        color: 'green',
+        fontSize: '50px'
+      }}>
+        To to App
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
